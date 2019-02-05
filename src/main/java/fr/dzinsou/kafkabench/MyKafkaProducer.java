bda5f78e8017ddcee47e47662fc8e1db7178e3f8
Dietrich Zinsou <dzinsou@hortonworks.com>
2019-02-05 14:51:42 +0100

    Init v0.2.0

--- src/main/java/fr/dzinsou/kafkabench/MyKafkaProducer.java
+++ src/main/java/fr/dzinsou/kafkabench/MyKafkaProducer.java
@@ -1,10 +1,14 @@
 package fr.dzinsou.kafkabench;
 
+import org.apache.commons.cli.*;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.kafka.clients.producer.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.Future;
 
@@ -13,27 +17,21 @@ public class MyKafkaProducer implements Closeable {
 
     private Producer<String, String> kafkaProducer;
 
-    private String kafkaBootstrapServers;
-    private String securityProtocol;
-    private String kafkaKrbServiceName;
+    private String producerPropertiesFilePath;
 
-    public MyKafkaProducer(String kafkaBootstrapServers, String securityProtocol, String kafkaKrbServiceName) {
-        this.kafkaBootstrapServers = kafkaBootstrapServers;
-        this.securityProtocol = securityProtocol;
-        this.kafkaKrbServiceName = kafkaKrbServiceName;
+    public MyKafkaProducer(String producerPropertiesFilePath) throws IOException {
+        this.producerPropertiesFilePath = producerPropertiesFilePath;
         this.init();
     }
 
-    private void init() {
-        Properties producerProps = new Properties();
-        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.kafkaBootstrapServers);
-        producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        producerProps.put("security.protocol", this.securityProtocol);
-        if (this.kafkaKrbServiceName != null && !this.kafkaKrbServiceName.equals("")) {
-            producerProps.put("sasl.kerberos.service.name", this.kafkaKrbServiceName);
+    private void init() throws IOException {
+        try (FileInputStream fis = new FileInputStream(this.producerPropertiesFilePath)) {
+            Properties producerProps = new Properties();
+            producerProps.load(fis);
+            producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+            producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+            this.kafkaProducer = new KafkaProducer<>(producerProps);
         }
-        this.kafkaProducer = new KafkaProducer<>(producerProps);
     }
 
     public Future<RecordMetadata> sendMessage(String topicName, String messageKey, String messageValue, Callback callback) {
@@ -43,22 +41,79 @@ public class MyKafkaProducer implements Closeable {
     @Override
     public void close() {
         if (this.kafkaProducer != null) {
-            LOGGER.info("Closing producer [{}]", this.kafkaBootstrapServers);
             this.kafkaProducer.close();
         }
     }
 
-    public static void main(String[] args) {
-        String kafkaBootstrapServers = args[0];
-        String securityProtocol = args[1];
-        String kafkaKrbServiceName = args[2];
-        String topicName = args[3];
-        int iterationsCount = Integer.parseInt(args[4]);
+    public static void main(String[] args) throws ParseException {
+        Options options = new Options();
+        options.addOption("producer_properties", true, "Producer properties file");
+        options.addOption("topic", true, "Kafka topic");
+        options.addOption("batch_message_size", true, "Batch message size (bytes)");
+        options.addOption("batch_message_count", true, "Batch message count per iteration");
+        options.addOption("batch_iteration_count", true, "Batch iteration count");
+        options.addOption("batch_iteration_pause", true, "Batch pause between iterations (milliseconds)");
 
-        try (MyKafkaProducer myKafkaProducer = new MyKafkaProducer(kafkaBootstrapServers, securityProtocol, kafkaKrbServiceName)) {
-            for (int i = 0; i < iterationsCount; i++) {
-                myKafkaProducer.sendMessage(topicName, null, "hello world", null);
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        String producerPropertiesFilePath = cmd.getOptionValue("producer_properties");
+        String topicName = cmd.getOptionValue("topic");
+        int batchMessageSize = Integer.parseInt(cmd.getOptionValue("batch_message_size"));
+        long batchMessageCount = Integer.parseInt(cmd.getOptionValue("batch_message_count"));
+        long batchIterationCount = Integer.parseInt(cmd.getOptionValue("batch_iteration_count"));
+        long batchIterationPauseMillis = Integer.parseInt(cmd.getOptionValue("batch_iteration_pause"));
+        String batchMessage = RandomStringUtils.randomAlphabetic(batchMessageSize);
+
+        LOGGER.info("producerPropertiesFilePath: [{}]", producerPropertiesFilePath);
+        LOGGER.info("batchMessageSize          : [{}]", batchMessageSize);
+        LOGGER.info("batchMessageCount         : [{}]", batchMessageCount);
+        LOGGER.info("batchIterationCount       : [{}]", batchIterationCount);
+        LOGGER.info("batchIterationPauseMillis : [{}]", batchIterationPauseMillis);
+        LOGGER.info("batchMessage              : [{}]", batchMessage);
+
+        try (MyKafkaProducer myKafkaProducer = new MyKafkaProducer(producerPropertiesFilePath)) {
+            MyKafkaCallback myKafkaCallback = new MyKafkaCallback();
+            for (int i = 0; i < batchIterationCount; i++) {
+                LOGGER.info("Iteration [{}]", i);
+                for (int j = 0; j < batchMessageCount; j++) {
+                    myKafkaProducer.sendMessage(topicName, "message"+j, batchMessage, myKafkaCallback);
+                    myKafkaCallback.incrementSent();
+                }
+                Thread.sleep(batchIterationPauseMillis);
             }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e.getCause());
+        }
+    }
+
+    /**
+     * Kafka callback
+     */
+    static class MyKafkaCallback implements Callback {
+        private final Logger LOGGER = LoggerFactory.getLogger(MyKafkaCallback.class);
+
+        private int counter = 0;
+        private int failed = 0;
+        private int succeeded = 0;
+        private int sent = 0;
+
+        @Override
+        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+            counter++;
+
+            if (e != null) {
+                failed++;
+                LOGGER.error(e.getMessage());
+            } else {
+                succeeded++;
+            }
+
+            if (counter % 1000 == 0) LOGGER.info("Status - [{}][{}][{}]", failed, succeeded, sent - counter);
+        }
+
+        private void incrementSent() {
+            sent++;
         }
     }
 }
